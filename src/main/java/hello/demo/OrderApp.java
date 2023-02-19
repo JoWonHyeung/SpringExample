a630855db5a35251dfb907e34a05c7f762ffb2fd
JoWonHyeung <mesh153@naver.com>
2023-02-19 23:26:51 +0900

    2023-02-19

--- src/main/java/hello/demo/OrderApp.java
+++ src/main/java/hello/demo/OrderApp.java
@@ -7,11 +7,17 @@ import hello.demo.member.MemberServiceImpl;
 import hello.demo.order.Order;
 import hello.demo.order.OrderService;
 import hello.demo.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
